d0f006be97b0d75815bbe73cb6f5c9b77dbb9771
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-03-27 23:32:30 -0600

    Agregar funcionalidad para mostrar el nombre y correo del usuario en el dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,16 @@
 import Counter from '@/components/Counter';
+import { prisma } from '@/lib/prisma';
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const user = await prisma.user.findFirst();
+
   return (
     <div className=" bg-zinc-50 font-sans dark:bg-blue-900">
       <h1>Dashboard</h1>
       <Counter />
+      <h2>
+        Welcome, {user.name}! Your email is {user.email}.
+      </h2>
     </div>
   );
 }
