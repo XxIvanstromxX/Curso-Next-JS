78e42afbd70da8868a380f5705549cbecfbdae77
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-03-28 23:35:50 -0600

    Eliminar lógica de usuario y componente Counter del Dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,16 +1,9 @@
-import Counter from '@/components/Counter';
 import { prisma } from '@/lib/prisma';
 
-export default async function Dashboard() {
-  const user = await prisma.user.findFirst();
-
+export default function Dashboard() {
   return (
-    <div className=" bg-zinc-50 font-sans dark:bg-blue-900">
+    <div className=" bg-zinc-50 font-sans dark:bg-blue-900 flex flex-col justify-center">
       <h1>Dashboard</h1>
-      <Counter />
-      <h2>
-        Welcome, {user.name}! Your email is {user.email}.
-      </h2>
     </div>
   );
 }
