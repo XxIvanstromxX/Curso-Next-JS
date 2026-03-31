bc7f88ed6eff05d28061779e18a9abc25ab8abb9
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-03-30 22:39:58 -0600

    Actualizar componente Dashboard para mostrar información del usuario autenticado

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,20 @@
-import { prisma } from '@/lib/prisma';
+import { auth } from '@/lib/auth';
+import { SignOut } from '@/components/SingOutButton';
+import Image from 'next/image';
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await auth();
+
   return (
     <div className=" bg-zinc-50 font-sans dark:bg-blue-900 flex flex-col justify-center">
       <h1>Dashboard</h1>
+      <p>Welcome, {session?.user?.name}</p>
+      <img
+        src={session?.user?.image}
+        alt="Imagen usuario"
+        className="rounded-full w-16 h-16"
+      />
+      <SignOut />
     </div>
   );
 }
