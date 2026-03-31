20b61e9e6054eeab9c05074deb46afdac65de68f
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-03-30 22:40:11 -0600

    Agregar archivo auth.js para implementar autenticación con NextAuth y GitHub

---
+++ src/lib/auth.js
@@ -0,0 +1,14 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import { prisma } from './prisma';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      return !!auth;
+    },
+  },
+});
